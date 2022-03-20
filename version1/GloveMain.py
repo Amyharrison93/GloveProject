94074f3f639bfe31395174191b401cf1f5bfd5d8
Your Name <you@example.com>
2022-03-20 03:13:31 +0000

    MILESTONE! this version does a useful thing please dont break it

--- version1/GloveMain.py
+++ version1/GloveMain.py
@@ -4,22 +4,39 @@ import os
 import sys
 import time
 from datetime import datetime
+from numpy import zeros
 sys.path.append(os.getcwd())
 
 import MPU6050Lib as mpu
 import rpiGPIOLib as gpio
+import GloveOperations as glvOp
 #import GraphOutLib as graph
 
 def main():
     '''
     Main program function
     '''
+    
+    nowTime = datetime.now()
+    
+    sensorCount : int = 4
+    sensorMain : int = 0
+    
+    sensor = [0,0,0]
+    accelData = [0,0,0]
+    gyroData = [0,0,0]
+    accelAngleData = [0,0,0]
+    gyroAngleData = [0,0,0]
+    gyroAngleData = [0,0,0]
+    
+    output = zeros(sensorCount)
+    
     #graph.TestPlot()
-    gpio.gpioSetup(3)
+    gpio.gpioSetup(sensorCount)
     gpio.pinInit()
     
-    ad0Pattern = mpu.ad0Init(3)
-    ad0PatternFlip = mpu.ad0Init(3)
+    ad0Pattern = mpu.ad0Init(sensorCount)
+    ad0PatternFlip = mpu.ad0Init(sensorCount)
     
     for i in range(0, len(ad0PatternFlip)):
         ad0PatternFlip[i] = abs(int(ad0PatternFlip[i]) - 1)
@@ -28,17 +45,8 @@ def main():
     time.sleep(0.1)
     
     imu1 = mpu.initSensor(0x68)
-    print(imu1)
     
     time.sleep(0.1)
-    
-    sensor = [0,0,0]
-    accelData = [0,0,0]
-    gyroData = [0,0,0]
-    accelAngleData = [0,0,0]
-    gyroAngleData = [0,0,0]
-    gyroAngleData = [0,0,0]
-    nowTime = datetime.now()
     
     while True:
         for j in range(0,len(ad0Pattern)):
@@ -68,13 +76,21 @@ def main():
                         gyroAngleData[i] = mpu.radToDeg(mpu.gyroAngle(gyroRead, deltaTime, gyroAngleData[i]))
                     except: 
                         gyroAngleData[i] = mpu.gyroAngle(gyroRead, deltaTime)
-                    print("sensor 1 abs roll = : {}, gyro data = {}, timeDelta = {}".format(gyroAngleData[1][0], gyroData[1][0], deltaTime))
+                    #print("sensor 1 abs roll = : {}, gyro data = {}, timeDelta = {}".format(gyroAngleData[1][0], gyroData[1][0], deltaTime))
                     
                 except Exception as e:
                     print(e)
                 
-                time.sleep(0.1)    
+                time.sleep(0.01)    
             sensor[j] = accelData, gyroData, gyroAngleData
+            
+            #for i in sensor:
+            #    if i == 1:
+            #        for j in sensor[sensorMain]:
+            #            output[i][j] = sensor[sensorMain][j]
+            #    else: 
+            #        output[i] = glvOp.calcoffset(sensor[sensorMain], sensor[i])
+            #print(output)
         
         #print("sensor value = {}".format(sensor[0]))        
         #graph.plotPoints3d(accelData)
